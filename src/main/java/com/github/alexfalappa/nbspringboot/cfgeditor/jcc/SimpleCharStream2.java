aafa23ab7a8f5b4e490fc9142203d889ad39e27d
Alessandro Falappa <alex.falappa@gmail.com>
2017-03-12 18:40:02 +0100

    Copied and tweaked generated jcc parser into source tree.
    Followed NetBeans jcc lexer integration tutorial.

---
+++ src/main/java/com/github/alexfalappa/nbspringboot/cfgeditor/jcc/SimpleCharStream2.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2017 Alessandro Falappa.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.alexfalappa.nbspringboot.cfgeditor.jcc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import org.netbeans.spi.lexer.LexerInput;
+
+/**
+ *
+ * @author Alessandro Falappa
+ */
+public class SimpleCharStream2 {
+
+    private LexerInput input;
+
+    static boolean staticFlag;
+
+    public SimpleCharStream2(LexerInput input) {
+        this.input = input;
+    }
+
+    SimpleCharStream2(Reader stream, int i, int i0) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    SimpleCharStream2(InputStream stream, String encoding, int i, int i0) throws UnsupportedEncodingException {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    char BeginToken() throws IOException {
+        return readChar();
+    }
+
+    String GetImage() {
+        return input.readText().toString();
+    }
+
+    public char[] GetSuffix(int len) {
+        if (len > input.readLength()) {
+            throw new IllegalArgumentException();
+        }
+        return input.readText(input.readLength() - len, input.readLength()).toString().toCharArray();
+    }
+
+    void ReInit(Reader stream, int i, int i0) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void ReInit(InputStream stream, String encoding, int i, int i0) throws UnsupportedEncodingException {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void backup(int i) {
+        input.backup(i);
+    }
+
+    int getBeginColumn() {
+        return 0;
+    }
+
+    int getBeginLine() {
+        return 0;
+    }
+
+    int getEndColumn() {
+        return 0;
+    }
+
+    int getEndLine() {
+        return 0;
+    }
+
+    char readChar() throws IOException {
+        int result = input.read();
+        if (result == LexerInput.EOF) {
+            throw new IOException("LexerInput EOF");
+        }
+        return (char) result;
+    }
+
+}
